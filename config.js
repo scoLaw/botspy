a09c96f98b9dd3e069dc8bc28a0ee93f6309344f
scoLaw <104954768+scoLaw@users.noreply.github.com>
2022-05-05 05:23:59 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+require("dotenv").config()
+const config = {
+  token: process.env.DISCORD_TOKEN,
+  adminIDs: ["885161189628325919", "229368840633450497", "290044931190554625"],
+  guildID: "885157348996444160",
+}
+module.exports = config
